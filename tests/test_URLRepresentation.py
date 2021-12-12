33fd87737d66d1cbcfc7ed6e4d0ad786969dea61
Enchan <enchant0130@gmail.com>
2021-12-12 16:59:59 +0900

    [Add #9] URL-repr testcode

---
+++ tests/test_URLRepresentation.py
@@ -0,0 +1,14 @@
+#
+#
+#
+from unittest import TestCase
+from termdecorator import termdecorate
+
+
+class testURLRepresentation(TestCase):
+
+    @termdecorate
+    def testURLRepresentation(self):
+        """ URL表現テスト """
+
+        pass
