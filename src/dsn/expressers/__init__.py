a69a6b789ab04b369b90fba029f0a242a2b86078
Enchan <enchant0130@gmail.com>
2021-12-13 15:48:51 +0900

    [Update #14] MySQL, SQLite

--- src/dsn/expressers/__init__.py
+++ src/dsn/expressers/__init__.py
@@ -4,6 +4,8 @@
 
 from .base_expr import Base
 from .http_expr import HTTPExpresser
+from .mysql_expr import MySQLExpresser
+from .sqlite_expr import SQLiteExpresser
 
 # Expresser一覧
 expressers = list(Base.expressers)
---
+++ src/dsn/expressers/mysql_expr.py
@@ -0,0 +1,44 @@
+#
+# MySQL DSN Expresser
+#
+from src.dsn import DSN
+from urllib.parse import urlunparse
+
+from .base_expr import Base
+
+
+class MySQLExpresser(Base):
+
+    """ MySQLのExpresser """
+
+    __pattern__: str = "^mysql$"
+
+    @staticmethod
+    def urlexpr(dsn: DSN) -> str:
+
+        # mysql://<user>:<password>@<host>:<port>/<database_path>?<options>
+
+        # 最低限 user, host は必要
+        if dsn.user is None or dsn.host is None:
+            raise ValueError("convert failed: MySQL requires user, host but includes None")
+
+        # pathが空文字なのもアウト
+        if dsn.path == "":
+            raise ValueError("convert failed: path must not be None or blank")
+
+        # netlocを生成
+
+        # ログインクレデンシャル部分
+        login_credential = dsn.user
+        if (dsn.password or "") != "":
+            login_credential += f":{dsn.password}"
+
+        # サーバ・ポート部分
+        serverloc = dsn.host
+        if dsn.port is not None:
+            serverloc += f":{dsn.port}"
+
+        netloc = f"{login_credential}@{serverloc}"
+
+        # urlunparseで戻す
+        return urlunparse((dsn.scheme, netloc, dsn.path, dsn.params, dsn.query, dsn.fragment))
